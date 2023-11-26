507737692d310829523ae4904b8bb574ddb568dc
Luv Gangwani <luv.gangwani@gmail.com>
2023-11-26 23:51:58 +1100

    Update default message

--- next-js/app/api/route.ts
+++ next-js/app/api/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     return NextResponse.json({
-        message: 'Project Management Tool API'
+        message: 'Project Management App API'
     })
 }
