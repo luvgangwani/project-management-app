f3e78dff19a0fc0570b1cb7591f8c4a0117beca6
Luv Gangwani <luv.gangwani@gmail.com>
2024-01-06 18:25:51 +1100

    Chnage font and add header placement.

--- next-js/app/page.tsx
+++ next-js/app/page.tsx
@@ -4,8 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header>Project Management App</header>
-      <Link href="/api/auth/signout">Logout</Link>
+      
     </main>
   )
 }
