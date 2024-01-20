5a0074afab72a021fcf870c656463c9d5ee47fba
Luv Gangwani <luv.gangwani@gmail.com>
2024-01-20 23:16:59 +1100

    Remove session references from header.

--- next-js/app/header.tsx
+++ next-js/app/header.tsx
@@ -1,11 +1,6 @@
-import { getServerSession } from "next-auth";
 import Link from "next/link";
-import authOptions from "./api/auth/[...nextauth]/auth-options";
 
 async function Header() {
-  const session = await getServerSession(authOptions);
-
-
   return (
     <header className="app-header">
       <span>Project Management App</span>
