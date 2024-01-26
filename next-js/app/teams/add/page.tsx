492657478aeced4602a1f0947f42c35dd4bb174b
Luv Gangwani <luv.gangwani@gmail.com>
2024-01-27 02:12:30 +1100

    Add a basic form for adding a new team.

--- next-js/app/teams/add/page.tsx
+++ next-js/app/teams/add/page.tsx
@@ -1,6 +1,24 @@
-function TeamAddForm() {
+import { Role } from "@/app/enums"
+import { getAuthUser } from "@/app/util"
+import styles from './page.module.css'
+
+async function TeamAddForm() {
+    const authUser = await getAuthUser()
   return (
-    <div>TeamAddForm</div>
+    (authUser?.role !== Role.ADMIN && authUser?.role !== Role.MANAGER)
+    ?
+    <div>You're not authorised to view this page.</div>
+    :
+    <div className={styles.container}>
+        <div className={styles.header}>Add a new team</div>
+        <form>
+            <label htmlFor="name">Name:</label>
+            <input type="text" name="name" id="name" />
+            <label htmlFor="description">Description:</label>
+            <textarea name="description" id="description"></textarea>
+            <button type="submit" id="btnCreate">Create</button>
+        </form>
+    </div>
   )
 }
 
