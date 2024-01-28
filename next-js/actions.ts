cbd4f49c14ec35f164084643e24a1452ba28863b
Luv Gangwani <luv.gangwani@gmail.com>
2024-01-28 13:29:57 +1100

    Form actions for new team creation.

---
+++ next-js/actions.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import TeamsController from "./app/controllers/Teams";
+import ProjectManagementAppAPIError from "./app/errors/ProjectManagementAppAPIError";
+import { ITeams } from "./app/interfaces";
+import { getAuthUser } from "./app/util";
+
+export async function createTeam(prev: { message: string }, formData: FormData) {
+    const teamsController = new TeamsController();
+
+    const authUser = await getAuthUser();
+
+    const team: ITeams = {
+        name: formData.get('name') as string,
+        description: formData.get('description') as string,
+        managerUsername: authUser?.username
+    };
+    try {
+        const response = await teamsController.create(team)
+        return response
+    } catch (error) {
+        const { message } = error as ProjectManagementAppAPIError
+        return {
+            message,
+        }
+    }
+}
--- next-js/app/teams/add/page.tsx
+++ next-js/app/teams/add/page.tsx
@@ -1,20 +1,27 @@
-import withAuthorize from '@/app/hoc/withAuthorize'
+"use client";
+
 import styles from './page.module.css'
+import { useFormState } from 'react-dom';
+import { createTeam } from '@/actions';
 
-
+const initialState = {
+  message: ''
+}
 function TeamAddForm() {
+  const [state, formAction] = useFormState(createTeam, initialState)
   return (
     <div className={styles.container}>
         <div className={styles.header}>Add a new team</div>
-        <form>
+        <form action={formAction}>
             <label htmlFor="name">Name: </label>
             <input type="text" name="name" id="name" />
             <label htmlFor="description">Description:</label>
             <textarea name="description" id="description"></textarea>
             <button type="submit" id="btnCreate">Create</button>
         </form>
+        <div className={styles.message}>{state.message}</div>
     </div>
   )
 }
 
-export default withAuthorize(TeamAddForm)
+export default TeamAddForm // adding withAuthorize causes unexpected failures. Try setting this from the controller instead
