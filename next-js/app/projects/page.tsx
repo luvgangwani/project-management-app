3960f4e806b630f813aae3c8fb310870cc02940b
Luv Gangwani <luv.gangwani@gmail.com>
2024-02-01 00:32:37 +1100

    Add basic pages for projects and tasks.

---
+++ next-js/app/projects/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+function Projects() {
+  return (
+    <div>Projects</div>
+  )
+}
+
+export default Projects
---
+++ next-js/app/tasks/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.css';
+
+function Tasks() {
+  return (
+    <div>Tasks</div>
+  )
+}
+
+export default Tasks
